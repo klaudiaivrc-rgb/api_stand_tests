3c6fb5f74aca720f35adf42ab7674b6b0d384b9a
Claudia Ivette Romero Cruz <klaudiaiv.rc@gmail.com>
2026-01-14 17:04:51 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = 'https://cnt-6a5d7d47-901f-4acf-9e51-9f900d329d08.containerhub.tripleten-services.com'
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ sender_stand_request.py
@@ -0,0 +1,17 @@
+import configuration
+import requests
+import data
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+def post_products_kits(products_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,
+                         json=products_ids,
+                         headers=data.headers)
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH,
+                        headers=data.headers)
